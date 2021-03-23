907c73b35220fbf679f86403af297a899a8c7090
Anmoldeep Singh Kang <asinghkang@avantisoft.com>
2021-03-22 21:04:40 -0600

    Updates

--- src/infrastructure/auth/AuthService.ts
+++ src/infrastructure/auth/AuthService.ts
@@ -25,7 +25,7 @@ export class AuthService {
         clientId: '959fcaaa-6a51-4e66-a643-b962f2405067',
         authority: 'https://wupiter.b2clogin.com/wupiter.onmicrosoft.com/B2C_1_signupsignin1',
         redirectUri: 'https://wupiter.azurewebsites.net/',
-        postLogoutRedirectUri: 'http://wupiter.azurewebsites.net/',
+        postLogoutRedirectUri: 'https://wupiter.azurewebsites.net/',
         navigateToLoginRequestUrl: true,
         validateAuthority:false
       },
